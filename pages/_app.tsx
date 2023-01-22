02313d7eedf1a1291098e12e00f7ceb4dcaaff35
dnhen <69449713+dnhen@users.noreply.github.com>
2023-01-22 17:42:12 +1100

    Added g analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,14 +4,30 @@ import { theme } from '@/theme';
 import { ChakraProvider } from '@chakra-ui/react';
 import '@fontsource/kanit';
 import type { AppProps } from 'next/app';
+import Script from 'next/script';
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <AuthProvider>
-      <ChakraProvider theme={theme}>
-        <Component {...pageProps} />
-      </ChakraProvider>
-    </AuthProvider>
+    <>
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+          
+            gtag('config', 'G-78VX14HL8E');
+          `
+        }}
+      />
+      <AuthProvider>
+        <ChakraProvider theme={theme}>
+          <Component {...pageProps} />
+        </ChakraProvider>
+      </AuthProvider>
+    </>
   );
 };
 

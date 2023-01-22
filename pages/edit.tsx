c48812f5ac39a78e7ede415f7253119e55866086
dnhen <69449713+dnhen@users.noreply.github.com>
2023-01-22 17:09:13 +1100

    Hidden flight date selector on edit flight page

--- pages/edit.tsx
+++ pages/edit.tsx
@@ -2,7 +2,7 @@ import { FlightTable } from '@/components/edit/FlightTable';
 import { Navbar } from '@/components/Navbar';
 import { StandardPage } from '@/components/StandardPage';
 import { withAuth } from '@/hoc/withAuth';
-import { Divider, FormControl, FormLabel, Heading } from '@chakra-ui/react';
+import { Divider, Heading } from '@chakra-ui/react';
 import Head from 'next/head';
 import { useState } from 'react';
 
@@ -21,10 +21,12 @@ const Edit = () => {
       <StandardPage>
         <Heading mb="2">Edit Flight</Heading>
         <Divider />
+        {/*}
         <FormControl>
           <FormLabel>Flights Date</FormLabel>
           <input type="date" onChange={(e) => setEditDate(new Date(new Date(e.target.value).setHours(0, 0, 0, 0)))} />
         </FormControl>
+        {*/}
         <FlightTable isEditable={true} displayDateStart={editDate} displayDateEnd={new Date(new Date(editDate).setHours(24, 0, 0, 0))} />
       </StandardPage>
     </>

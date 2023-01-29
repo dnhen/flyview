cfbc1a0b73b92eda7568f9dee5372019c3a88c78
dnhen <69449713+dnhen@users.noreply.github.com>
2023-01-29 18:22:46 +1100

    Added animation

--- components/edit/FlightTable.tsx
+++ components/edit/FlightTable.tsx
@@ -1,8 +1,7 @@
 import { useAuth } from '@/contexts/AuthContext';
 import { useFlights } from '@/hooks/useFlights';
-import { MINUTES_AFTER_DEP_TO_DISPLAY } from '@/pages/viewer';
-import { Editable, EditableInput, EditablePreview, HStack, IconButton, PinInput, PinInputField, Table, Tbody, Td, Th, Thead, Tr } from '@chakra-ui/react';
-import { FaTrash } from 'react-icons/fa';
+import { Table, Tbody, Th, Thead, Tr } from '@chakra-ui/react';
+import { FlightRow } from './FlightRow';
 
 interface FlightTableProps {
   isEditable?: boolean;
@@ -13,7 +12,7 @@ interface FlightTableProps {
 export const FlightTable = ({ isEditable = false }: FlightTableProps) => {
   const { currentUser } = useAuth();
   const airlineCode = currentUser!.uid; // Use the user's UID for airline code; TODO: update this to not be UID
-  const { flights, updateFlight, deleteFlight } = useFlights(airlineCode);
+  const { flights } = useFlights(airlineCode);
 
   return (
     <Table variant="striped">
@@ -30,64 +29,7 @@ export const FlightTable = ({ isEditable = false }: FlightTableProps) => {
       </Thead>
       <Tbody>
         {flights.map((flight, i) => {
-          const timezoneOffset = new Date().getTimezoneOffset() * 60000;
-          const departureTimeDate = flight.data.actualDepartureTime.toDate();
-          const timeBeforeToViewFlight = new Date();
-          timeBeforeToViewFlight.setMinutes(timeBeforeToViewFlight.getMinutes() - MINUTES_AFTER_DEP_TO_DISPLAY);
-          const noLongerVisible = departureTimeDate < timeBeforeToViewFlight;
-
-          return (
-            <Tr key={i} maxH="2" borderX="4px" borderColor={noLongerVisible ? 'red.600' : 'green.600'}>
-              <Td>
-                <Editable isDisabled={!isEditable} placeholder={flight.data.flightNumber} defaultValue={flight.data.flightNumber} onSubmit={(newValue) => newValue && updateFlight(flight.ref, { flightNumber: newValue })}>
-                  <EditablePreview />
-                  <EditableInput />
-                </Editable>
-              </Td>
-              <Td>
-                <Editable isDisabled={!isEditable} placeholder={flight.data.destination} defaultValue={flight.data.destination} onSubmit={(newValue) => newValue && updateFlight(flight.ref, { destination: newValue })}>
-                  <EditablePreview />
-                  <EditableInput />
-                </Editable>
-              </Td>
-              <Td>
-                <input
-                  disabled={!isEditable}
-                  value={new Date(flight.data.actualDepartureTime.toDate() - timezoneOffset).toISOString().slice(0, 19)}
-                  type="datetime-local"
-                  onChange={(e) => e.target.value && updateFlight(flight.ref, { actualDepartureTime: new Date(e.target.value) })}
-                />
-              </Td>
-              <Td>
-                <input
-                  disabled={!isEditable}
-                  value={new Date(flight.data.actualBoardingTime.toDate() - timezoneOffset).toISOString().slice(0, 19)}
-                  type="datetime-local"
-                  onChange={(e) => e.target.value && updateFlight(flight.ref, { actualBoardingTime: new Date(e.target.value) })}
-                />
-              </Td>
-              <Td>
-                <HStack>
-                  <PinInput isDisabled={!isEditable} defaultValue={flight.data.gate.toString()} onChange={(newValue) => newValue && updateFlight(flight.ref, { gate: parseInt(newValue) })}>
-                    <PinInputField />
-                    <PinInputField />
-                    <PinInputField />
-                  </PinInput>
-                </HStack>
-              </Td>
-              <Td>
-                <Editable isDisabled={!isEditable} placeholder="empty" defaultValue={flight.data.remark} onSubmit={(newValue) => updateFlight(flight.ref, { remark: newValue })}>
-                  <EditablePreview />
-                  <EditableInput />
-                </Editable>
-              </Td>
-              {isEditable && (
-                <Td>
-                  <IconButton aria-label={'delete'} icon={<FaTrash />} border="1px" borderColor="red.600" color="red.600" onClick={() => deleteFlight(flight.ref)} />
-                </Td>
-              )}
-            </Tr>
-          );
+          return <FlightRow key={i} flight={flight} isEditable={isEditable} />;
         })}
       </Tbody>
     </Table>

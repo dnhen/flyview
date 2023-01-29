dcc45a241459d7a88694bf7320278024fbfb8970
dnhen <69449713+dnhen@users.noreply.github.com>
2023-01-29 16:23:16 +1100

    Added verification

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,9 @@ import { Head, Html, Main, NextScript } from 'next/document';
 const Document = () => {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="google-site-verification" content="QOCt272LKtfOSSzMUsX9hPDuxo4mpvgM6IRUbfl2SAk" />
+      </Head>
       <body>
         <Main />
         <NextScript />

b9b19afb35b73f5bb328a0c77b2dc5992a2f60d5
dnhen <69449713+dnhen@users.noreply.github.com>
2023-01-30 18:40:21 +1100

    Loading colours

--- components/LoadingScreen.tsx
+++ components/LoadingScreen.tsx
@@ -9,8 +9,8 @@ export const LoadingScreen = () => {
         <title>flyview</title>
       </Head>
       <Flex flexDir="column" w="100vw" h="100vh" alignItems="center" justifyContent="center">
-        <Icon as={LogoIcon} boxSize="12" pos="absolute" />
-        <Spinner boxSize="16" />
+        <Icon as={LogoIcon} boxSize="12" pos="absolute" color="jet.600" />
+        <Spinner boxSize="16" color="jet.600" />
       </Flex>
     </>
   );

83158adb0ee56493e56a7042374a66e82b1c7a39
dnhen <69449713+dnhen@users.noreply.github.com>
2023-01-30 18:35:20 +1100

    Changed loading screen, made login screen nicer

--- components/LoadingScreen.tsx
+++ components/LoadingScreen.tsx
@@ -1,9 +1,11 @@
-import { Flex, Spinner } from '@chakra-ui/react';
+import { Flex, Icon, Spinner } from '@chakra-ui/react';
+import { LogoIcon } from './LogoIcon';
 
 export const LoadingScreen = () => {
   return (
-    <Flex w="100vw" h="100vh" alignItems="center" justifyContent="center">
-      <Spinner size="xl" />
+    <Flex flexDir="column" w="100vw" h="100vh" alignItems="center" justifyContent="center">
+      <Icon as={LogoIcon} boxSize="12" pos="absolute" />
+      <Spinner boxSize="16" />
     </Flex>
   );
 };
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,7 @@ const Document = () => {
   return (
     <Html lang="en">
       <Head>
+        <title>flyview</title>
         <meta name="google-site-verification" content="QOCt272LKtfOSSzMUsX9hPDuxo4mpvgM6IRUbfl2SAk" />
       </Head>
       <body>
